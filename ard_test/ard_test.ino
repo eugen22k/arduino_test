55bb0e6d03d3c10a109ad01f322b189541773b8e
Eugen22k <eugen22k@mail.ru>
2018-06-26 09:50:28 +0400

    Старт проекта

---
+++ ard_test/ard_test.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       ard_test.ino
+    Created:	26.06.2018 9:39:00
+    Author:     SOFTLINE\lyskovtsevey
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
