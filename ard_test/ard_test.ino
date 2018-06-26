712b07e7fa1b2f5b738b0d37ec735db42b568756
Eugen22k <eugen22k@mail.ru>
2018-06-26 10:33:56 +0400

    Добавил наработки

--- ard_test/ard_test.ino
+++ ard_test/ard_test.ino
@@ -2,31 +2,98 @@
 // 
 /*
     Name:       ard_test.ino
-    Created:	26.06.2018 9:39:00
-    Author:     lyskovtsevey
+    Created:	
+    Author:     eugen22k
 */
 
-// Define User Types below here or use a .h file
-//
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <SPI.h>
+#include "Ucglib.h"
+#include <TimeLib.h>
+#include <DS1307RTC.h>  // a basic DS1307 library that returns time as a time_t
 
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
+Adafruit_BMP085 bmp;
+Ucglib_ILI9163_18x128x128_SWSPI ucg(/*sclk=*/ 13, /*data=*/ 11, /*cd=*/ 9, /*cs=*/ 10, /*reset=*/ 8);
 
+float temp = 0;
 
-// Define Functions below here or use other .ino or cpp files
-//
+void setup() {
+	Serial.begin(9600);
+	while (!Serial); // Needed for Leonardo only
+	setSyncProvider(RTC.get);   // the function to get the time from the RTC
+	if (timeStatus() != timeSet)
+		Serial.println("Unable to sync with the RTC");
+	else
+		Serial.println("RTC has set the system time");
 
-// The setup() function runs once each time the micro-controller starts
-void setup()
-{
-
-
+	bmp.begin();
+	delay(1000);
+	ucg.begin(UCG_FONT_MODE_SOLID);
+	ucg.clearScreen();
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
-{
+void loop() {
+	if (Serial.available()) {
+		time_t t = processSyncMessage();
+		if (t != 0) {
+			RTC.set(t);   // set the RTC and the system time to the received value
+			setTime(t);
+		}
+	}
+	digitalClockDisplay();
 
+	ucg.setRotate180();
+	ucg.setFont(ucg_font_helvB08_tr);
+	ucg.setColor(255, 255, 255);
 
+	ucg.setPrintPos(0, 25);
+	ucg.print("Temp = ");
+	ucg.print(bmp.readTemperature());
+	ucg.println(" *C");
+
+	ucg.setPrintPos(0, 50);
+	ucg.print("Pressure = ");
+	ucg.print(bmp.readPressure());
+	ucg.println(" Pa");
+}
+
+void digitalClockDisplay() {
+	ucg.setPrintPos(0, 75);
+	// digital clock display of the time
+	ucg.print(hour());
+	printDigits(minute());
+	// printDigits(second());
+	ucg.setPrintPos(0, 90);
+	ucg.print(day());
+	ucg.print(" ");
+	ucg.print(month());
+	ucg.print(" ");
+	ucg.print(year());
+}
+
+void printDigits(int digits) {
+	// utility function for digital clock display: prints preceding colon and leading 0
+	ucg.print(":");
+	if (digits < 10)
+		ucg.print('0');
+	ucg.print(digits);
+}
+
+/*  code to process time sync messages from the serial port   */
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+
+unsigned long processSyncMessage() {
+	unsigned long pctime = 0L;
+	const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
+
+	if (Serial.find(TIME_HEADER)) {
+		pctime = Serial.parseInt();
+		return pctime;
+		if (pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+			pctime = 0L; // return 0 to indicate that the time is not valid
+		}
+	}
+	return pctime;
 }
